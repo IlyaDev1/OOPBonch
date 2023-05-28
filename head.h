470e4807da4419a3e069c211e10a91907e4fcb68
IliDevia <neiluamaibi@mail.ru>
2023-05-28 15:33:56 +0300

    newLab2

--- head.h
+++ head.h
@@ -2,156 +2,132 @@
 using namespace std;
 
 
-class ClientBank{
+class COne;
+class CTwo;
+
+class COne{
 protected:
-    string BankName;
-    class ClientBalance{
-    protected:
-        float Balance;
-
-    public:
-        ClientBalance(){
-            this->Balance = 0;
-            cout << "Balance: " << this << endl;
-        }
-
-        ClientBalance(float value){
-            this->Balance = value;
-            cout << "Balance: " << this << endl;
-        }
-
-        ClientBalance & operator =(ClientBalance & object){
-            this->Balance = object.Balance;
-            cout << "Balance: " << this << endl;
-        }
-
-        float getBalance(){
-            return Balance;
-        }
-
-        void setBalance(float value){
-            this->Balance = value;
-        }
-
-        void print(){
-            cout << "Balance: " << Balance << endl;
-        }
-
-        ~ClientBalance(){
-            cout << "Balance: " << this << endl;
-        }
-    };
-    ClientBalance CliBalance;
+    friend CTwo;
+    double D;
+    string S;
 
 public:
-    ClientBank(){
-        this->BankName = "Bank";
-        cout << "Bank: " << this << endl;
+    COne(){
+        this->D = 0;
+        this->S = "";
+        cout << "created: " << this << endl;
     }
 
-    ClientBank(string name, float BalanceValue=0){
-        this->BankName = name;
-        CliBalance.setBalance(BalanceValue);
-        cout << "Bank: " << this << endl;
+    COne(double d, string s){
+        this->D = d;
+        this->S = s;
+        cout << "created: " << this << endl;
     }
 
-    ClientBank(ClientBank & object){
-        this->BankName = object.BankName;
-        this->CliBalance.setBalance(object.CliBalance.getBalance());
-        cout << "Bank: " << this << endl;
+    COne(const COne & object){
+        this->D = object.D;
+        this->S = object.S;
+        cout << "created: " << this << endl;
     }
 
-    ClientBank & operator =(ClientBank & object){
-        this->BankName = object.BankName;
-        this->CliBalance.setBalance(object.CliBalance.getBalance());
-        cout << "Bank: " << this << endl;
+    COne & operator =(const COne & object){
+        this->D = object.D;
+        this->S = object.S;
+        cout << "created: " << this << endl;
     }
 
-    string getBankName(){
-        return BankName;
+    void setD(double d){
+        this->D = d;
     }
 
-    float getClientBalance(){
-        return CliBalance.getBalance();
+    void setS(string s){
+        this->S = s;
     }
 
-    void setBankName(string name){
-        this->BankName = name;
+    double getD(){
+        return D;
     }
 
-    void setClientBalance(float ClientBalanceValue){
-        this->CliBalance.setBalance(ClientBalanceValue);
+    string getS(){
+        return S;
     }
 
     void print(){
-        cout << "Bank: " << BankName << endl;
-        CliBalance.print();
+        cout << "D: " << D << "  ||  " << "S: " << S << "  ||  ";
     }
 
-    ~ClientBank(){
-        cout << "Bank: " << this << endl;
+    ~COne(){
+        cout << "deleted: " << this << endl;
     }
 };
 
 
-class SavingAccount : public ClientBank{
-private:
-    float SavingBalance;
-    bool OutputPermission = false;
+class CTwo{
+protected:
+    int N;
+    COne *One;
 
 public:
-    SavingAccount(){
-        this->SavingBalance = 0;
-        cout << "SavingAccount: " << this << endl;
+    CTwo(){
+        this->N = 0;
+        this->One = new COne;
+        cout << "created2: " << this << endl;
     }
 
-    SavingAccount(float value, string name="Bank", float BalanceOfClient=0){
-        this->SavingBalance = value;
-        this->BankName = name;
-        this->CliBalance.setBalance(BalanceOfClient);
-        cout << "SavingAccount: " << this << endl;
+    CTwo(int n, double d=0, string s=""){
+        this->N = n;
+        this->One = new COne(d, s);
+        cout << "created2: " << this << endl;
     }
 
-    SavingAccount(SavingAccount & object){
-        this->SavingBalance = object.SavingBalance;
-        this->BankName = object.BankName;
-        this->CliBalance.setBalance(object.CliBalance.getBalance());
-        cout << "SavingAccount: " << this << endl;
+    CTwo(const CTwo & object){
+        this->N = object.N;
+        this->One = new COne(*object.One);
+        cout << "created2: " << this << endl;
     }
 
-    SavingAccount & operator =(SavingAccount & object){
-        this->SavingBalance = object.SavingBalance;
-        this->BankName = object.BankName;
-        this->CliBalance.setBalance(object.CliBalance.getBalance());
-        cout << "SavingAccount: " << this << endl;
+    CTwo & operator =(const CTwo & object){
+        this->N = object.N;
+        this->One = new COne(*object.One);
+        cout << "created2: " << this << endl;
     }
 
-    void setPermission(bool value){
-        this->OutputPermission = value;
-        cout << "for output you need to wait a day" << endl;
+    void setN(int n){
+        this->N = n;
+    }
+    void setD(double D);
+    void setS(string S);
+
+    int getN(){
+        return N;
+    }
+    double getD();
+    string getS();
+
+    void print(){
+        One->print();
+        cout << "N: " << N << endl;
     }
 
-    float getSavingBalance(){
-        return SavingBalance;
-    }
-
-    void setSavingBalance(float value){
-        if (not OutputPermission){
-            cout << "operation is not available" << endl;
-        }
-        else{
-            this->SavingBalance = value;
-            OutputPermission = false;
-        }
-    }
-
-    void printSA(){
-        cout << "Saving Balance: " << SavingBalance << endl;
-        cout << "OutputPermission: " << OutputPermission << endl;
-        print();
-    }
-
-    ~SavingAccount(){
-        cout << this << endl;
+    ~CTwo(){
+        delete One;
+        cout << "deleted2: " << this << endl;
     }
 };
+
+
+void CTwo::setD(double d){
+    One->setD(d);
+}
+
+void CTwo::setS(string s){
+    One->setS(s);
+}
+
+double CTwo::getD(){
+    return One->getD();
+}
+
+string CTwo::getS(){
+    return One->getS();
+}
--- main.cpp
+++ main.cpp
@@ -1,8 +1,8 @@
 #include "head.h"
 
 
-void decoration(){
-    for (int i=0; i<20; i++){
+void decor(){
+    for (int i=0;i<20;i++){
         cout << "=";
     }
     cout << endl;
@@ -10,48 +10,25 @@ void decoration(){
 
 
 int main(){
-    decoration();
-    ClientBank user1;
-    ClientBank user2("tinkoff");
-    ClientBank user3("sber", 100);
-    ClientBank user4(user2);
-    ClientBank user5 = user3;
-    decoration();
+    decor();
+    CTwo a;
+    CTwo b(20, 3.14, "hi");
+    CTwo c(b);
+    CTwo d = b;
+    a.print();
+    b.print();
+    c.print();
+    d.print();
+    decor();
 
-    decoration();
-    user1.print();
-    user2.print();
-    user3.print();
-    user4.print();
-    user5.print();
-    decoration();
-
-    decoration();
-    cout << user3.getBankName() << endl;
-    cout << user3.getClientBalance() << endl;
-    user3.setBankName("NewTestBank");
-    user3.setClientBalance(99999);
-    cout << user3.getBankName() << endl;
-    cout << user3.getClientBalance() << endl;
-    decoration();
-
-    cout << endl << endl << endl;
-
-    decoration();
-    SavingAccount a1;
-    SavingAccount a2(12300, "tinkoff", 20000);
-    SavingAccount a3(a2);
-    SavingAccount a4 = a1;
-    decoration();
-
-    decoration();
-    a2.printSA();
-    a2.setPermission(true);
-    a2.setSavingBalance(123456);
-    cout << a2.getSavingBalance() << endl;
-    a2.setSavingBalance(123);
-    a2.printSA();
-    decoration();
+    decor();
+    c.setN(40);
+    c.setD(13.5);
+    c.setS("newS");
+    cout << c.getN() << endl;
+    cout << c.getD() << endl;
+    cout << c.getS() << endl;
+    decor();
 
     return 0;
 }
